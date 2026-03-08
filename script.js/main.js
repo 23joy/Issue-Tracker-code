41bfd842d5fd4a370159560c3ae405a563c0cbfe
23joy <joyshreeroy288@gmail.com>
2026-03-08 05:18:41 -0700

    add the button section and javascript funtion

--- script.js/main.js
+++ script.js/main.js
@@ -1,5 +1,27 @@
-document.getElementById("btn-search").addEventListener("click",function(){
-    const input=document.getElementById("input-search")
-    const searchValue=input.value
-    console.log(searchValue);
-})
+let currentTab='all';
+const tabActive=["bg-blue-900",'border-blue-900',"text-white"]
+const tabInActive=["bg-white","text-slate-700","border-slate-200"];
+function switchTab(tab){
+    currentTab=tab;
+
+    const tabs=['all','open','close']
+
+    for(const t of tabs){
+        const tabName=document.getElementById("tab-" + t );
+        if(t===tab){
+            tabName.classList.remove(...tabInActive)
+            tabName.classList.add(...tabActive)
+        }
+        else{
+            tabName.classList.remove(...tabActive)
+            tabName.classList.add(...tabInActive)
+        }
+    }
+}
+switchTab(currentTab);
+
+// document.getElementById("btn-search").addEventListener("click",function(){
+//     const input=document.getElementById("input-search")
+//     const searchValue=input.value
+//     console.log(searchValue);
+// })
